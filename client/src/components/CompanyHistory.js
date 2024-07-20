d659c71f358afb08cd3657ade3e5c1cd7015ad14
Pratigya <pratigyadhakal4444@gmail.com>
2024-07-20 19:32:22 +0545

    [UI]:Company History

---
+++ client/src/components/CompanyHistory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/CompanyHistory.css';
+
+const CompanyHistory = () => {
+    return (
+        <section className="company-history">
+        <h2>Our History</h2>
+        <p>Nepali Sparsa was founded with the vision of bringing the finest Nepali crafts to the global market. Our journey began with a small group of artisans...</p>
+        {/* Add more content as per the design */}
+        </section>
+    );
+}
+
+export default CompanyHistory;
+
