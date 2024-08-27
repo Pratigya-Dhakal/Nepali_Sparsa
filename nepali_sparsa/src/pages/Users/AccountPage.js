4a632ac7003e4e4b70832f30b9b758d1c4424eef
Pratigya <pratigyadhakal4444@gmail.com>
2024-08-27 22:15:09 +0545

    Account page WIP

--- nepali_sparsa/src/pages/Users/AccountPage.js
+++ nepali_sparsa/src/pages/Users/AccountPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Sidebar from '../../components/UserComponents/profile/Sidebar';
+import UserCard from '../../components/UserComponents/profile/UserCard';
+import AddressCard from '../../components/UserComponents/profile/AddressCard';
+import OrderCard from '../../components/UserComponents/profile/OrderCard';
 import './styles/AccountPage.css';
 import Navbar from '../../components/UserComponents/Navbar';
 
@@ -10,7 +13,13 @@ const AccountPage = () => {
             <div className="account-page">
                 <Sidebar />
                 <div className="account-content">
-                    {/* Other components like UserCard, AddressCard, and OrderCard will go here */}
+                    <div className="cards-section">
+                        <UserCard />
+                        <AddressCard />
+                    </div>
+                    <div className="orders-section">
+                        <OrderCard />
+                    </div>
                 </div>
             </div>
         </div>
