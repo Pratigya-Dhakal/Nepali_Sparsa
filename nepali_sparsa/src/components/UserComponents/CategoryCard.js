d695b847ee5dd363303b85b72156e3e4d48b6e13
Pratigya <pratigyadhakal4444@gmail.com>
2024-08-28 22:49:55 +0545

    WIP: category card

--- nepali_sparsa/src/components/UserComponents/CategoryCard.js
+++ nepali_sparsa/src/components/UserComponents/CategoryCard.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './styles/CategoryCard.css'; // Adjust path as needed
+import { useNavigate } from 'react-router-dom';
 
 const CategoryCard = ({ category }) => {
+    const navigate = useNavigate();
+
+    const handleCategoryClick = () => {
+        navigate(`/shop?category=${category.name}`);
+    };
+
     const imageUrl = category.image ? `http://localhost:5000/${category.image.url}` : '/path/to/default-image.jpg';
 
     return (
-        <div className="category-card">
+        <div className="category-card" onClick={handleCategoryClick}>
             <img 
                 src={imageUrl} 
                 alt={category.name} 
